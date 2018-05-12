cb6b02cdca10b0ff4d61338c57004305b5a0d723
asriadi-PC\asriadi <asriadikhonza@gmail.com>
2018-05-12 17:27:56 +0700

    progres_counter

---
+++ Counter.java
@@ -0,0 +1,41 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Counter here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Counter extends Actor
+{
+    /**
+     * Act - do whatever the Counter wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+     private int value = 0;
+    private int target = 0;
+    private String text;
+    private int stringLength;
+    
+    public Counter()
+    {
+        this("");
+    }
+
+    public Counter(String prefix)
+    {
+        text = prefix;
+        stringLength = (text.length() + 2) * 16;
+
+        setImage(new GreenfootImage(stringLength, 24));
+        GreenfootImage image = getImage();
+        Font font = image.getFont();
+        image.setFont(font.deriveFont(24.0F));  // use larger font
+        
+        updateImage();
+    }
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
